251df9d9ac9b158a30573621a61fa824f643d943
dibahk <dibahadjikazemi@gmail.com>
2025-10-18 15:35:39 +0100

    parameter tuning

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import  GridSearchCV
 
 from src.exception import CustomException
 
@@ -13,5 +15,24 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_model(x, y, models, params):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv= 3)
+            gs.fit(x, y)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x, y)
+            y_pred = model.predict(x)
+            report[list(models.keys())[i]] = r2_score(y, y_pred)
+            
+        return report
     except Exception as e:
         raise CustomException(e, sys)
